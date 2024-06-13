4e9a6b18f1c6c18015abce553e9987c07e723b9c
surajkrdas <viralvirat902@gmail.com>
2024-06-13 22:43:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 4000;
 
-// In-memory data store
 let posts = [
   {
     id: 1,
@@ -34,26 +33,22 @@ let posts = [
 
 let lastId = 3;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Write your code here//
 
-// CHALLENGE 1: GET All posts
 app.get('/posts', (req, res) => {
   console.log(posts);
   res.json(posts);
 });
 
-//CHALLENGE 2: GET a specific post by id
 app.get('/posts/:id', (req, res) => {
   const id=parseInt(req.params.id);
   const ans=posts.find((post)=> post.id===id);
   if (!ans) return res.status(404).json({ message: "Post not found" });
   res.json(ans);
 });
-//CHALLENGE 3: POST a new post
+
 app.post("/posts",(req,res)=>{
   const newPost={
     id:posts.length+1,
@@ -67,7 +62,6 @@ app.post("/posts",(req,res)=>{
   res.status(201).json(newPost);
 });
 
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
 app.patch("/posts/:id",(req,res)=>{
   const post=posts.find((p)=>p.id===parseInt(req.params.id));
   if(!post)res.sendStatus(404).json({message:"Could not Patch"});
@@ -77,7 +71,7 @@ app.patch("/posts/:id",(req,res)=>{
   console.log(post);
   res.json(post);
 });
-//CHALLENGE 5: DELETE a specific post by providing the post id.
+
 app.delete("/posts/:id",(req,res)=>{
   const id=parseInt(req.params.id);
   const ind=posts.findIndex((post)=> post.id===id);
